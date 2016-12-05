44c013f0ccbd21bafa08e42ac8a8c37b6cb231c0
arseny010 <avsarsen@gmail.com>
2016-12-04 18:08:29 -0600

    Update gps2_2.rb

--- gps2_2.rb
+++ gps2_2.rb
@@ -1,23 +1,26 @@
+# create a method to make a new hash
 def create_list(str)
+  # make spaces between keys of the hash
   keys = str.split(' ')
   grocery_list = Hash.new 
   keys.each do |item|
     p grocery_list[item] = 1 
   end 
-  
   return grocery_list
 end 
-
+#print list of groceries
 final_list = create_list("carrot eggs milk")
-
+#define a method which add new items to the list
 def add(final_list, item, quan)
   p final_list[item] = quan
 end
+#define a method which removes items from the list
  def remove(final_list, item)
    final_list.delete(item)
  end
+  #define a method which updates items from the list
  def change(final_list, item, quan)
-   final_list.merge!(quan)
+   p final_list[item] = quan
  end
 p final_list
 add(final_list, "oranges", 3)
